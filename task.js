50b1659588f763ea16a2d9af4eb32f1fc73cdfa7
Arsen4ik <azalaradle@gmail.com>
2019-10-18 21:43:19 +0300

    beginning task app

---
+++ task.js
@@ -0,0 +1,13 @@
+var Task = function (name) {
+    this.name = name;
+    this.completed = false;
+}
+
+Task.prototype.complete = function () {
+    console.log('completing task: ' + this.name);
+    this.completed = true;
+}
+
+Task.prototype.save = function () {
+    console.log('saving task: ' + this.name)
+}
